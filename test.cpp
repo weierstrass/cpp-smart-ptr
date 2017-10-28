9a732dbff2c6db0a0cae8d5585997d9a9b855cc6
Weierstrass <andreas@bulling.se>
2017-10-28 19:43:11 +0200

    Add move constructor to unique_ptr

--- test.cpp
+++ test.cpp
@@ -7,6 +7,8 @@
 static int destruct_count(0);
 
 struct Dummy {
+    Dummy(int val) : val(val) {}
+    Dummy() : val(0) {}
     int val;
     ~Dummy(){destruct_count++;}
 };
@@ -35,4 +37,17 @@ TEST(test, unique_ptr_get) {
 TEST(test, unique_ptr_operator_star) {
     ab::unique_ptr<int> ptr(new int(5));
     EXPECT_EQ(5, *ptr);
+}
+
+TEST(test, unique_ptr_operator_arrow) {
+    ab::unique_ptr<Dummy> ptr(new Dummy(5));
+    EXPECT_EQ(5, ptr->val);
+}
+
+TEST(test, unique_ptr_move) {
+    ab::unique_ptr<int> ptr(new int(5));
+    ab::unique_ptr<int> ptr2(std::move(ptr));
+
+    EXPECT_EQ(5, *ptr2);
+    if (ptr) FAIL();
 }
--- unique_ptr.hpp
+++ unique_ptr.hpp
@@ -6,24 +6,41 @@ namespace ab {
     template<typename T>
     class unique_ptr {
     public:
-        unique_ptr(T* ptr) : ptr(ptr) {}
+        explicit unique_ptr(T* ptr) : ptr(ptr) {}
+        
         unique_ptr() : ptr(0) {}
+
+        unique_ptr(unique_ptr<T>&& ptr) : ptr(ptr.get()) {
+            ptr.reset();
+        }
+
+        unique_ptr(const unique_ptr<T>& ptr) = delete;
 
         ~unique_ptr() {
             delete ptr;
+        }
+
+        T& operator*() const {
+            assert(ptr);
+            return *ptr;
+        }
+
+        T* operator->() const {
+            return ptr;
         }
 
         explicit operator bool() const {
             return ptr != 0;
         }
 
+        unique_ptr<T>& operator=(const unique_ptr<T>& ptr) = delete;
+
         T* get() const {
             return ptr;
         }
 
-        T& operator*() const {
-            assert(ptr);
-            return *ptr;
+        void reset() {
+            ptr = 0;
         }
 
     private:
